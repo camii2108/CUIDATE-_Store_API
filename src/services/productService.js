038411ccc9d6150d644ff8e26fa6fb63570797ac
unknown <salasfrancog@gmail.com>
2023-05-17 23:32:41 -0300

    arreglando ramas

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,1 +1,11 @@
-.
+const { Product } = require("../database/models");
+
+const getProducts = async () => {
+    try {
+        const products = await Product.findAll({
+            include: [
+                { association: /* TE LO DEJO HASTA ACA...*/},
+            ]
+        })
+    }
+}
